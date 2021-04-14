1e6925c072fd6c045aeb8ff8be368e9a3e87c5ba
Moecreeper <Moecreeper@gmail.com>
2021-04-14 09:46:14 +0800

    study

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,22 @@
-    const para = document.querySelector('p');
-    para.addEventListener('click', updateName);
-    function updateName() {
-      const name = prompt('输入一个新的名字：');
-      para.textContent = '玩家1：' + name;
-    }
-/*     如果脚本无需等待页面解析，且无依赖独立运行，那么应使用 async。
-如果脚本需要等待页面解析，且依赖于其它脚本，调用这些脚本时应使用 defer，
-将关联的脚本按所需顺序置于 HTML 中。 */
+var timeGame=0;
+var timeWIn=0;
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值 
+}
+function randomGame(){
+var a=getRandomIntInclusive(1,9);
+var b=getRandomIntInclusive(1,9);
+var c=getRandomIntInclusive(1,9);
+console.log(a,b,c);
+if(a===8||b===8||c===8)
+{
+  timeGame++;
+  timeWIn++;
+  window.alert("你赢了Ծ‸ Ծ 你的战绩"+timeWIn+"/"+timeGame);}
+else{
+  timeGame++;
+window.alert("我赢了ﾟ∀ﾟ)σ 你的战绩"+timeWIn+"/"+timeGame);
+}
+}
